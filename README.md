de0de355eb836775a20f2b6a092fa5884fc702b4
06kellyjac <jack@control-plane.io>
2019-12-09 18:19:34 +0000

    Added the build script and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Kubernetes JSON Schemas Container
 
 Putting the Kubernetes JSON Schemas into containers for use in building containers
+
+To use the schemas in your containers you can add the following:
+
+```Dockerfile
+COPY --from=controlplane/kubernetes-json-schema:master-standalone / /some/location
+```
+
+Just change the tag from `master-standalone` to whichever schema you want.
+
+To see all possible tags look on the [docker hub tags page][docker_hub_tags].
+
+The build script will contain the last 3 semver minor versions as is the kubernetes support policy.
+But there will be previously buit versions in the tag list from when they were supported.
+
+[docker_hub_tags]: https://hub.docker.com/r/controlplane/ipfs-cluster-test/tags
---
+++ build.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+# set -euxo pipefail
+
+SCHEMA_REPO="https://github.com/instrumenta/kubernetes-json-schema.git"
+REPO_OUTPUT="./kubernetes-json-schema"
+IMAGE="06kellyjac/tmp"
+
+BUILD_ALL_VERSIONS=${1:-false}
+
+cleanup() {
+	if [ -d "$REPO_OUTPUT" ]; then
+		rm -rf "$REPO_OUTPUT"
+	fi
+}
+
+clone_schemas() {
+	git clone "$SCHEMA_REPO" "$REPO_OUTPUT"
+	echo
+}
+
+list_standalone_versions() {
+	find . -type d -name "*-standalone*"
+}
+
+sort_semver() {
+	sed "/-/!{s/$/_/}" | sort -Vr | sed "s/_$//"
+}
+
+extract_dir() {
+	REGEX_DIR="v\d+\.\d+\.\d+.*|master.*"
+	grep -Po "$REGEX_DIR"
+}
+
+extract_version() {
+	REGEX_VERSION="v\d+\.\d+\.\d+"
+	grep -Po "$REGEX_VERSION"
+}
+
+# Function agument has a fallback default
+# shellcheck disable=SC2120
+latest_three_minor() {
+	# Accept first argument or default to any didgets
+	MAJOR="${1:-\d}"
+	REGEX_MINOR="v${MAJOR}+\.\d+"
+	grep -Po "$REGEX_MINOR" | uniq | sed 3q
+}
+
+matching_patches() {
+	all="$1"
+	latest=$(echo "$2" | tr '\n' '|')
+	# Removing the additional '|' at the end
+	latest="${latest%?}"
+
+	echo "$all" | grep -P "$latest"
+}
+
+get_docker_tags() {
+	URL="https://hub.docker.com/v2/repositories/${IMAGE}/tags"
+	RESPONSE=$(curl -s "$URL")
+	echo "$RESPONSE" | jq --raw-output ".results[].name"
+}
+
+docker_contain() {
+	CONTEXT="$1"
+	TAG="$2"
+
+	echo docker build "$CONTEXT" -f Dockerfile -t "$TAG"
+	echo "Success"
+	echo "Pushing image '$TAG' to Docker Hub ..."
+	echo docker push "$TAG"
+	echo "Success"
+	echo
+}
+
+build_missing_docker_tags() {
+	DOCKER_TAGS="$1"
+	VERSIONS="$2"
+
+	for VERSION in $VERSIONS; do
+		is_match=false
+		for TAG in $DOCKER_TAGS; do
+			if [ "$TAG" = "$VERSION" ]; then
+				is_match=true
+			fi
+		done
+
+		if $is_match; then
+			echo "Tag '$VERSION' already exists on image '$IMAGE', skipping ..."
+			echo
+		else
+			echo "Tag '$VERSION' does not exist on image '$IMAGE', building ..."
+			FULL_TAG="${IMAGE}:${VERSION}"
+			docker_contain "$REPO_OUTPUT/${VERSION}" "$FULL_TAG"
+		fi
+	done
+}
+
+# A pinned build of master for existing kubesec use
+build_pinned_legacy_version() {
+	DOCKER_TAGS="$1"
+	PINNED_TAG="kubesec_v2_pinned"
+	COMMIT_SHA="8aa572595b98d73b2b9415ca576f78e163381b10"
+
+	( cd "$REPO_OUTPUT" && git branch && git checkout "$COMMIT_SHA" --quiet && git branch && echo "" )
+
+	is_match=false
+	for TAG in $DOCKER_TAGS; do
+		if [ "$TAG" = "$PINNED_TAG" ]; then
+			is_match=true
+			return
+		fi
+	done
+
+	if $is_match; then
+		echo "Tag '$PINNED_TAG' already exists on image '$IMAGE', skipping ..."
+		echo
+	else
+		echo "Tag '$PINNED_TAG' does not exist on image '$IMAGE', building ..."
+		FULL_TAG="${IMAGE}:${PINNED_TAG}"
+		docker_contain "$REPO_OUTPUT/master-standalone" "$FULL_TAG"
+	fi
+}
+
+
+trap cleanup EXIT SIGINT SIGQUIT
+cleanup
+clone_schemas
+
+SORTED_SEMVER=$(list_standalone_versions | sort_semver)
+VERSIONS_DIR=$(echo "$SORTED_SEMVER" | extract_dir)
+VERSIONS=$(echo "$SORTED_SEMVER" | extract_version)
+
+if ! $BUILD_ALL_VERSIONS; then
+	VERSIONS=$(echo "$VERSIONS" | latest_three_minor)
+fi
+
+# Attach master as a required version to build
+VERSIONS="$VERSIONS
+master"
+
+TARGET_VERSIONS=$(matching_patches "$VERSIONS_DIR" "$VERSIONS")
+echo "Target Versions:"
+echo "${TARGET_VERSIONS}"
+echo
+
+DOCKER_TAGS=$(get_docker_tags)
+build_missing_docker_tags "$DOCKER_TAGS" "$TARGET_VERSIONS"
+
+build_pinned_legacy_version "$DOCKER_TAGS"
